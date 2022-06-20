ca1efefdda6b8aa02bcf9bec4378aa1c6a87c92a
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-06-20 09:01:20 +0000

    [odd]6-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy,] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create,:destroy]
-  end  
+  end
   
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
