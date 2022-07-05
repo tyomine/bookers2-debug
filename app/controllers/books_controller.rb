024530d31b7232a077c7baa245db7f295017a314
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-05 01:19:55 +0000

    [add]詳細ページに閲覧数追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
     @user = @book.user
+    @book_detail = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.view_counts.create(book_id: @book_detail.id)
+    end 
   end
 
   def index
