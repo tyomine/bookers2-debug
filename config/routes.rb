98c46bfc6214ebfbf530a2250bf67ae5b11f6a5c
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-11 05:41:25 +0000

    [add]日付検索による投稿数の表示

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    get 'search', to: "users#search"
   end
   
   resources :chats, only: [:show, :create]
